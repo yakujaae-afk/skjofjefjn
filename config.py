1fa79b9a7c9d8ab70c7c4a8e58b00dd87111fd3a
yakujaae-afk <yakujaae@gmail.com>
2026-04-27 04:44:52 -0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6618274486:AAESKaMTJJ0tOR62pqOPt-a0WcfpTi0y7nk")
-    API_ID = int(os.environ.get("API_ID", "20089019"))
-    API_HASH = os.environ.get("API_HASH", "0b2eb6aa72d5983fa9e4b8c77791c9c4")
-    AUTH_USERS = os.environ.get("AUTH_USERS", "6404676836")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8714143838:AAHy_RyVRchPHxHMOd5w_ssmImCQsH-XHmg")
+    API_ID = int(os.environ.get("API_ID", "30296254"))
+    API_HASH = os.environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "7660916897")
